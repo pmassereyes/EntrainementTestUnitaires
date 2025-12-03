351e9b2d88ce1e2186696c0e01a7d0a5b7a2a193
maspa5z <pablomassereyes935@gmail.com>
2025-12-03 21:09:34 +0100

    Classes Cure et WeekEnd

--- src/main/java/sio/devoir1sio1a/Model/Exo1/Cure.java
+++ src/main/java/sio/devoir1sio1a/Model/Exo1/Cure.java
@@ -1,7 +1,25 @@
 package sio.devoir1sio1a.Model.Exo1;
 
-public class Cure
+public class Cure extends Prestation
 {
+    private int tauxRemboursement;
+
+    public Cure(int idPrestation, String nomPrestation, int tauxRemboursement) {
+        super(idPrestation, nomPrestation);
+    }
+
+    @Override
+    public double getPrix() {
+        return super.getPrix();
+    }
+    @Override
+    public String getInfos(){
+        return super.getInfos();
+    }
+    public int getTauxRemboursement() {
+        return tauxRemboursement;
+    }
+
     // A vous de jouer
 
 }
--- src/main/java/sio/devoir1sio1a/Model/Exo1/WeekEnd.java
+++ src/main/java/sio/devoir1sio1a/Model/Exo1/WeekEnd.java
@@ -1,6 +1,19 @@
 package sio.devoir1sio1a.Model.Exo1;
 
-public class WeekEnd
+public class WeekEnd extends Prestation
 {
+    private String typeChambre;
+
+    public WeekEnd(int idPrestation, String nomPrestation, String typeChambre) {
+        super(idPrestation, nomPrestation);
+    }
+    @Override
+    public String getInfos(){
+        return super.getInfos();
+    }
+    @Override
+    public double getPrix(){
+        return super.getPrix();
+    }
     // A vous de jouer
 }
